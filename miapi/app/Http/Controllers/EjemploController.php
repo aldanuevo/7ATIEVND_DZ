c81f8c1cec6ef85d54e95162baf6031f1d0a67cc
caudillo sanchez jaime <20193l001117@utcv.edu.mx>
2021-09-04 16:49:53 -0500

    Mi primera ruta

---
+++ miapi/app/Http/Controllers/EjemploController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class EjemploController extends Controller
+{
+    public function index(Request $request){
+        return "hola ".$request->get("nombre");
+    }
+}
