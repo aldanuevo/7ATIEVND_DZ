58406fdbe4c5d02c22557b1a151d33e5360e8994
Rosales_Carrillo_Alondra <20193l001133@utcv.edu.mx>
2021-09-04 16:11:50 -0500

    mi primer ruta

---
+++ miapi/app/Http/Controllers/EjemploController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class EjemploController extends Controller
+{
+    public function index(Request $request){
+        return "Hola".$request->get("nombre");
+    }
+
+}
